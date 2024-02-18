027086ac16018dc38b4471d5b790f6863930aeaa
Ricardo Cedraz <emanuel.cedrazta@hotmail.com>
2024-02-17 21:31:21 -0300

    add postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345',
+      database: 'zip-code',
+      entities: [],
+      synchronize: true, 
+    }),
+],
   controllers: [AppController],
   providers: [AppService],
 })
