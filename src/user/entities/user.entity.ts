bbdda7eb5d0233fdbb12856e2ce3b80c5ac26a46
Lucas Assis <lukas.ecomp@gmail.com>
2024-02-25 22:40:02 -0300

    update gitignore to ignore vscode settings

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,31 +1,35 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
-    constructor(partial: Partial<User>) {
-        Object.assign(this, partial);
-    }
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
 
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({ nullable: true })
-    firstName: string;
+  @Column({ nullable: true })
+  firstName: string;
 
+  @Column({ nullable: true })
+  lastName: string;
 
-    @Column({ nullable: true })
-    lastName: string;
+  @Column({ unique: true })
+  email: string;
 
-    @Column({ unique: true })
-    email: string;
+  @Column({ nullable: true })
+  password: string;
 
-    @Column({ nullable: true })
-    password: string;
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
 
-    @CreateDateColumn({ type: 'timestamptz' })
-    createdAt: Date;
-
-    @UpdateDateColumn({ type: 'timestamptz' })
-    updatedAt: Date;
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt: Date;
 }
-
