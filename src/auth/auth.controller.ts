65a85f787270ff9b0e77a15b124402da2a0d1494
Lucas Assis <lukas.ecomp@gmail.com>
2024-03-02 17:46:58 -0300

    change expected variable from username to email at login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,11 +3,11 @@ import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService){}
+  constructor(private authService: AuthService) {}
 
-    @HttpCode(HttpStatus.OK)
-    @Post('login')
-    singIn(@Body() singInDTO: Record<string, any>){
-        return this.authService.singIn(singInDTO.username, singInDTO.password);
-    }
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  singIn(@Body() singInDTO: Record<string, any>) {
+    return this.authService.singIn(singInDTO.email, singInDTO.password);
+  }
 }
