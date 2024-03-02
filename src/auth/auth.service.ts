e24c0e2c473595d7c734dea1cd97df66250de601
Lucas Assis <lukas.ecomp@gmail.com>
2024-03-02 17:46:20 -0300

    avoid create an empty variable password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,14 +3,16 @@ import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {}
 
-    async singIn(email: string, pass: string): Promise<any> {
-        const user = await this.userService.findOneByEmail(email);
-        if (user?.password !== pass) {
-            throw new UnauthorizedException();
-        }
-        const { password, ...result } = user;
-        return result;
+  async singIn(email: string, pass: string): Promise<any> {
+    const user = await this.userService.findOneByEmail(email);
+
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
     }
+    delete user.password;
+
+    return user;
+  }
 }
