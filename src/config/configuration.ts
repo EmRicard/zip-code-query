ddecc2668a6de2ef744f0968709826d03d5925e7
Lucas Assis <lukas.ecomp@gmail.com>
2024-03-03 13:12:10 -0300

    add expiration and other jwt configurations for env var

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -10,6 +10,10 @@ export default () => ({
     useSSL: 'true' === process.env.DB_USE_SSL,
   },
   jwt: {
+    global: 'true' === process.env.JWT_IS_GLOBAL,
     secret: process.env.JWT_SECRET,
-  }
+    signOptions: {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    },
+  },
 });
