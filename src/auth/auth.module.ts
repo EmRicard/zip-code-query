8e7cf16a9c4ed9488dad771008b178dd5aa3f9fc
Ricardo Cedraz <emanuel.cedrazta@hotmail.com>
2024-03-03 12:54:05 -0300

    add jwt module registration to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,17 @@ import { AuthService } from './auth.service';
 import { UserService } from 'src/user/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import configuration from 'src/config/configuration';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService, UserService],
-  imports: [TypeOrmModule.forFeature([User])]
+  imports: [TypeOrmModule.forFeature([User]),
+  JwtModule.register({
+    global: true,
+    secret: configuration().jwt.secret,
+    signOptions: { expiresIn: '60s' },
+  }),]
 })
 export class AuthModule { }
