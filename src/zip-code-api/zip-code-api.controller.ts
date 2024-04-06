8facd72861a9f649394da4de3a815599a39576ed
Ricardo Cedraz <emanuel.cedrazta@hotmail.com>
2024-04-05 22:46:02 -0300

    add zip-code-api

--- src/zip-code-api/zip-code-api.controller.ts
+++ src/zip-code-api/zip-code-api.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ZipCodeApiService } from './zip-code-api.service';
 
 @Controller('zip-code-api')
 export class ZipCodeApiController {
   constructor(private readonly zipCodeApiService: ZipCodeApiService) {}
+
+  @Get()
+  async findAll(): Promise<any>{
+    return this.zipCodeApiService.findAll();
+  }
 }
--- src/zip-code-api/zip-code-api.service.ts
+++ src/zip-code-api/zip-code-api.service.ts
@@ -1,19 +1,21 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { AxiosError, AxiosResponse } from 'axios';
+import { Observable, catchError, firstValueFrom, lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class ZipCodeApiService {
+  private readonly logger = new logger(ZipCodeApiService);
   constructor(private readonly httpService: HttpService) {}
 
-  async findAll(): Promise<Cat[]> {
+  async findAll(): Promise<any[]>{ 
     const { data } = await firstValueFrom(
-      this.httpService.get<Cat[]>('http://localhost:3000/cats').pipe(
+      this.httpService.get<any[]>('http://viacep.com.br/ws/01001000/json/').pipe(
         catchError((error: AxiosError) => {
           this.logger.error(error.response.data);
           throw 'An error happened!';
         }),
       ),
     );
-    return data;
   }
 }
